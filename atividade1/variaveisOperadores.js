875024d4fd8d5a510988ca6481d2fad977c7fabb
ARTHUR ALVES DE SOUSA <arthur.asousa@a.ucb.br>
2024-03-13 09:43:44 -0300

    Atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+/*Faça a declaração de 2 variáveis numéricas para cada operador e guarde o resultado da operação em uma terceira variável;
+ Para cada resultado obtido, imprima o texto “Resultado da {operação} entre {num_A} e {num_B}” */
+
+ let a = 20
+ let b = 5
+
+ soma = a + b
+ sub = a - b
+ mult = a * b
+ div = a / b
+
+ console.log('Resultado da soma entre a e b', soma)
+ console.log('Resultado da subtração entre a e b', sub)
+ console.log('Resultado da multiplicação entre a e b', mult)
+ console.log('Resultado da divisão entre a e b', div)
