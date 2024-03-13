5959398c8f1341eca38f0b46f1a9351f994eb21b
Arthur <111136613+ArthurAlvesz@users.noreply.github.com>
2024-03-13 09:58:04 -0300

    Atividade 1

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -9,7 +9,7 @@
  mult = a * b
  div = a / b
 
- console.log('Resultado da soma entre a e b', soma)
- console.log('Resultado da subtração entre a e b', sub)
- console.log('Resultado da multiplicação entre a e b', mult)
- console.log('Resultado da divisão entre a e b', div)
+ console.log('Resultado da soma entre'+ a +' e '+ b + 'é' + soma)
+ console.log('Resultado da subtração entre '+ a +' e '+ b + 'é' + sub)
+ console.log('Resultado da multiplicação entre '+ a +' e '+ b + 'é' + mult)
+ console.log('Resultado da divisão entre '+ a +' e '+ b + 'é' + div)
